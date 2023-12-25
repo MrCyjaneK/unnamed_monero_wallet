2e06ff9ca26c930cc7687f346153b2ed4beda2e8
Czarek Nakamoto <cyjan@mrcyjanek.net>
2023-12-25 18:44:06 +0100

    fix: simplify font config

--- lib/const/resource.g.dart
+++ lib/const/resource.g.dart
@@ -7,59 +7,7 @@ class R {
   /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/anon_logo.png)
   static const String ASSETS_ANON_LOGO_PNG = 'assets/anon_logo.png';
 
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-Bold.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_BOLD_TTF =
-      'assets/fonts/RobotoMono-Bold.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-BoldItalic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_BOLDITALIC_TTF =
-      'assets/fonts/RobotoMono-BoldItalic.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-ExtraLight.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_EXTRALIGHT_TTF =
-      'assets/fonts/RobotoMono-ExtraLight.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-ExtraLightItalic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_EXTRALIGHTITALIC_TTF =
-      'assets/fonts/RobotoMono-ExtraLightItalic.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-Italic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_ITALIC_TTF =
-      'assets/fonts/RobotoMono-Italic.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-Light.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_LIGHT_TTF =
-      'assets/fonts/RobotoMono-Light.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-LightItalic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_LIGHTITALIC_TTF =
-      'assets/fonts/RobotoMono-LightItalic.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-Medium.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_MEDIUM_TTF =
-      'assets/fonts/RobotoMono-Medium.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-MediumItalic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_MEDIUMITALIC_TTF =
-      'assets/fonts/RobotoMono-MediumItalic.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-Regular.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_REGULAR_TTF =
-      'assets/fonts/RobotoMono-Regular.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-SemiBold.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_SEMIBOLD_TTF =
-      'assets/fonts/RobotoMono-SemiBold.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-SemiBoldItalic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_SEMIBOLDITALIC_TTF =
-      'assets/fonts/RobotoMono-SemiBoldItalic.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-Thin.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_THIN_TTF =
-      'assets/fonts/RobotoMono-Thin.ttf';
-
-  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono-ThinItalic.ttf)
-  static const String ASSETS_FONTS_ROBOTOMONO_THINITALIC_TTF =
-      'assets/fonts/RobotoMono-ThinItalic.ttf';
+  /// ![preview](file:///home/user/go/src/git.mrcyjanek.net/mrcyjanek/anonero/assets/fonts/RobotoMono.ttf)
+  static const String ASSETS_FONTS_ROBOTOMONO_TTF =
+      'assets/fonts/RobotoMono.ttf';
 }
