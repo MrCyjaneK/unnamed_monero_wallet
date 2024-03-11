c773eff456d3a1dfffa64643ddfbcc3b1545e2a9
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-03-11 23:56:36 +0100

    monero: bump to v0.18.3.2 sailfishos: use non-sfossdk build. android: drop i686 support (temporary)

--- build_changelog.sh
+++ build_changelog.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 LANG=C TZ=UTC git log --pretty=format:'{^^^^date^^^^:^^^^%ci^^^^,^^^^hash^^^^:^^^^%H^^^^,^^^^body^^^^:^^^^%B^^^^, ^^^^name^^^^: ^^^^%an <%ae>^^^^}&&&&' | awk '{printf "%s\\n", $0}' | sed 's/&&&&\\n/\n/g' | sed 's/"/\\"/g' | sed 's/\^^^^/"/g' > assets/changelog.jsonp
+flutter pub get
+dart pub add offline_market_data --hosted-url=https://git.mrcyjanek.net/api/packages/mrcyjanek/pub/
+make lib/helpers/licenses.g.dart
