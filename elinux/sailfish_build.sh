f691edf3caf0776dac410356376a30ddb81e869b
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-06-04 15:27:49 +0200

    sfos support (better)

--- elinux/sailfish_build.sh
+++ elinux/sailfish_build.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+set -e
 set -x
 cd $(dirname $0)
 cd ..
 
-flutter-elinux pub get
-flutter-elinux build elinux --release 
-curl https://static.mrcyjanek.net/monero_c/$(grep MONERO_C_TAG Makefile | head -1 | tr = ' ' | awk '{ print $2 }')/aarch64-linux-gnu_libwallet2_api_c.so.xz --output $PWD/build/elinux/arm64/release/bundle/lib/libwallet2_api_c.so.xz
-unxz $PWD/build/elinux/arm64/release/bundle/lib/libwallet2_api_c.so.xz
-cp $HOME/SailfishOS/flutter-elinux/$(uname -m)/flutter-client build/elinux/arm64/release/bundle/flutter-client
+FLUTTER_ARCH=""
+if [[ "$(uname -m)" == "aarch64" ]];
+then
+    FLUTTER_ARCH="arm64"
+fi        curl -L https://github.com/sony/flutter-embedded-linux/releases/download/c4cd48e186/elinux-arm64-release.zip --output elinux-arm64-release.zip
 
-cat > build/elinux/arm64/release/bundle/unnamed_monero_wallet <<EOF
+
+flutter-elinux pub get
+flutter-elinux build elinux --release
+
+./build_moneroc.sh --prebuild --coin monero --tag ${MONERO_C_TAG} --triplet $(uname -m)-linux-gnu --location $PWD/build/elinux/${FLUTTER_ARCH}/release/bundle/lib/
+
+cp $HOME/SailfishOS/flutter-elinux/$(uname -m)/flutter-client build/elinux/${FLUTTER_ARCH}/release/bundle/flutter-client
+
+cat > build/elinux/${FLUTTER_ARCH}/release/bundle/unnamed_monero_wallet <<EOF
 #!/bin/bash
 cd \$(dirname \$0)
 killall flutter-client || true
 
 LD_PRELOAD=\$PWD/lib/libflutter_engine.so ./flutter-client --bundle=\$PWD --fullscreen --force-scale-factor=3
 EOF
-chmod +x build/elinux/arm64/release/bundle/unnamed_monero_wallet
 
-rpmbuild -bb elinux/sailfishos.spec --define "_bundledir $PWD/build/elinux/arm64/release/bundle/" --define "_sourcedir $PWD"
-cp $HOME/rpmbuild/RPMS/aarch64/unnamed-monero-wallet-debuginfo-1.0.0+*.rpm build/elinux/arm64/release/unnamed-monero-wallet-debuginfo.aarch64.rpm
-cp $HOME/rpmbuild/RPMS/aarch64/unnamed-monero-wallet-debugsource-1.0.0+*.rpm build/elinux/arm64/release/unnamed-monero-wallet-debugsource.aarch64.rpm
-cp $HOME/rpmbuild/RPMS/aarch64/unnamed-monero-wallet-1.0.0+*.rpm build/elinux/arm64/release/unnamed-monero-wallet.aarch64.rpm
+chmod +x build/elinux/${FLUTTER_ARCH}/release/bundle/unnamed_monero_wallet
+
+rpmbuild -bb elinux/sailfishos.spec --define "_bundledir $PWD/build/elinux/${FLUTTER_ARCH}/release/bundle/" --define "_sourcedir $PWD"
+cp $HOME/rpmbuild/RPMS/$(uname -m)/unnamed-monero-wallet-debuginfo-1.0.0+*.rpm build/elinux/${FLUTTER_ARCH}/release/unnamed-monero-wallet-debuginfo.$(uname -m).rpm
+cp $HOME/rpmbuild/RPMS/$(uname -m)/unnamed-monero-wallet-debugsource-1.0.0+*.rpm build/elinux/${FLUTTER_ARCH}/release/unnamed-monero-wallet-debugsource.$(uname -m).rpm
+cp $HOME/rpmbuild/RPMS/$(uname -m)/unnamed-monero-wallet-1.0.0+*.rpm build/elinux/${FLUTTER_ARCH}/release/unnamed-monero-wallet.$(uname -m).rpm
--- elinux/sailfish_prepare.sh
+++ elinux/sailfish_prepare.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -e
+set -x
 
 zypper in -y ffmpeg-tools git clang libxkbcommon-devel wayland-protocols-devel wayland-client wayland-egl-devel make glibc-static
 mkdir -p $HOME/flutter-elinux
 git clone https://github.com/sony/flutter-elinux.git $HOME/flutter-elinux/$(uname -m)
+
+FVM_VERSION=$(cat .fvmrc  | grep flutter | xargs | awk '{ print $2 }')
+pushd "$HOME/flutter-elinux/$(uname -m)"
+    git checkout $FVM_VERSION
+popd
 
 echo 'export PATH="$PATH:$HOME/flutter-elinux/$(uname -m)/bin"' >> $HOME/.bashrc
 echo 'export PATH="$PATH:$HOME/flutter-elinux/$(uname -m)/flutter/bin"' >> $HOME/.bashrc
@@ -10,6 +17,7 @@ echo 'export PATH="$PATH:$HOME/.pub-cache/bin"' >> $HOME/.bashrc
 
 git config --global --add safe.directory $HOME/flutter-elinux/$(uname -m)/flutter
 git config --global --add safe.directory $HOME/flutter-elinux/$(uname -m)
+git config --global --add safe.directory '*' # screw it.
 
 # not-so-temporary fix
 curl -L --output /usr/include/linux/input-event-codes.h https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/input-event-codes.h
@@ -22,10 +30,11 @@ then
         git clone https://github.com/MrCyjaneK/flutter-embedded-linux
         cd flutter-embedded-linux
         mkdir build && cd build
+        # hash in here doesn't matter, it is just to make the compiler happy.
         curl -L https://github.com/sony/flutter-embedded-linux/releases/download/c4cd48e186/elinux-arm64-release.zip --output elinux-arm64-release.zip
         unzip elinux-arm64-release.zip && rm elinux-arm64-release.zip
         cmake ..
-        make
+        make -j$(nproc)
         mkdir -p $HOME/SailfishOS/flutter-elinux/$(uname -m)/
         cp flutter-client $HOME/SailfishOS/flutter-elinux/$(uname -m)/flutter-client
     popd
